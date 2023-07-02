dc1dc5c09748bac8d48e4ec1e0115bee2df37d49
Arthur Delannoy <ah.delannoy@gmail.com>
2023-07-02 19:48:53 +0200

    added median blur

--- transformermanager.py
+++ transformermanager.py
@@ -18,6 +18,7 @@ class TransformerManager():
             self.alert_colorspacechange,
             self.alert_simple_blur,
             self.alert_gaussian_blur,
+            self.alert_median_blur,
             self.alert_threshold1d,
             self.alert_adaptive_threshold1d
         ]
@@ -127,12 +128,17 @@ class TransformerManager():
         key_command = list(self.main_window.transformer.commands.keys())[8]
         self.transformation_saver(key_command)
 
-    def alert_threshold1d(self):
+    def alert_median_blur(self):
         """Send a string to "transformation_saver" that contains info about the new transformation"""
         key_command = list(self.main_window.transformer.commands.keys())[9]
         self.transformation_saver(key_command)
 
-    def alert_adaptive_threshold1d(self):
+    def alert_threshold1d(self):
         """Send a string to "transformation_saver" that contains info about the new transformation"""
         key_command = list(self.main_window.transformer.commands.keys())[10]
+        self.transformation_saver(key_command)
+
+    def alert_adaptive_threshold1d(self):
+        """Send a string to "transformation_saver" that contains info about the new transformation"""
+        key_command = list(self.main_window.transformer.commands.keys())[11]
         self.transformation_saver(key_command)
